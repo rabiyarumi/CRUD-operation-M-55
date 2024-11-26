6ba0525d867dd37e3859e4860c12c595b152bf19
rabiyarumi <rabiyaarumi@gmail.com>
2024-11-27 02:27:27 +0600

    added user deleted function

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,35 +1,42 @@
-import { useLoaderData } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
 
-    const users = useLoaderData();
+  //delete a user
+  const handleDelete = (_id) => {
+    console.log("Delete", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("user deleted successfully");
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      });
+  };
 
-    //delete a user
-    const handleDelete = _id => {
-        console.log('Delete', _id);
-        fetch(`http://localhost:5000/users/${_id}`, {
-            method: "DELETE"
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.deletedCount > 0){
-                alert("user deleted successfully")
-            }
-        })
-    }
-
-    return (
-        <div>
-            <h2>{users.length}</h2>
-            <div>
-                {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email} : {user._id} : 
-                    <button onClick={() => handleDelete(user._id)}>X</button></p>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2>{users.length}</h2>
+      <div>
+        {users.map((user) => (
+          <p key={user._id}>
+            {user.name} : {user.email} : {user._id} :
+            <Link to={`/update/${user._id}`}>
+            <button>Update</button></Link> : 
+            <button onClick={() => handleDelete(user._id)}>X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
